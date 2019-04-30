141b02d7a91df4f7f1b2c042b11538437376dce8
Sonu Kumar Singh <singhsonu778@gmail.com>
2019-04-30 17:47:07 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        reply('hello hapi');
+    }
+});
+
+server.start(() => console.log('Server running on %s', server.info.uri));
